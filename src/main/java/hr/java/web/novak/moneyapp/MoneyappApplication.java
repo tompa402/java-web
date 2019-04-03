7acf9eb63e42cba0ea383d7dbb31b5eba4fe841c
tompa402 <tomislavno@gmail.com>
2019-04-03 16:55:24 +0200

    This is where it all begins...

---
+++ src/main/java/hr/java/web/novak/moneyapp/MoneyappApplication.java
@@ -0,0 +1,13 @@
+package hr.java.web.novak.moneyapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoneyappApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoneyappApplication.class, args);
+    }
+
+}
