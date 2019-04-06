467ea2718594113baf911c51d20c3638829dde14
tompa402 <tomislavno@gmail.com>
2019-04-06 09:43:04 +0200

    prepare appplication for lab-03

---
+++ src/main/java/hr/java/web/novak/moneyapp/configuration/SecurityConfig.java
@@ -0,0 +1,44 @@
+package hr.java.web.novak.moneyapp.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin").password(passwordEncoder().encode("adminpass")).roles("ADMIN", "USER")
+                .and()
+                .withUser("student").password(passwordEncoder().encode("studentpass")).roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/expense/*").hasRole("USER")
+                .antMatchers("/expense*").hasRole("USER")
+                .antMatchers("/login*").permitAll()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/expense", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login");
+
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
